ab9e285365818149510b21309f364b22a13efde3
xuetaolu <36478973+xuetaolu@users.noreply.github.com>
2023-08-29 17:03:56 +0800

    原神云渲染

---
+++ Assets/Scenes/shaderDevelop_2023_08_10/原神云渲染/genship_cloud_common.hlsl
@@ -0,0 +1,33 @@
+#ifndef GENSHIP_CLOUD_COMMON_INCLUDED
+#define GENSHIP_CLOUD_COMMON_INCLUDED
+#include "UnityCG.cginc"
+
+float4 GlslToDxClipPos(float4 clipPos) {
+    clipPos.y = -clipPos.y;
+    clipPos.z = -0.5*clipPos.z + 0.5*clipPos.w;
+    return clipPos;
+}
+
+struct appdata
+{
+    float4 vertex : POSITION;
+    float4 color : COLOR;
+    float2 uv : TEXCOORD0;
+    float2 uv2 : TEXCOORD1;
+    float2 uv3 : TEXCOORD2;
+};
+
+struct v2f
+{
+    float4 vertex : SV_POSITION;
+    float4 Varying_0 : TEXCOORD0;
+    float4 Varying_1 : TEXCOORD1;
+    float4 Varying_2 : TEXCOORD2;
+    float3 Varying_3 : TEXCOORD3;
+    float3 Varying_4 : TEXCOORD4;
+    float3 Varying_5 : TEXCOORD5;
+    float3 Varying_6 : TEXCOORD6;
+    float3 Varying_7 : TEXCOORD7;
+};
+
+#endif
