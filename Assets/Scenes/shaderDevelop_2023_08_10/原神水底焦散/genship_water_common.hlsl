2b4872856af66c3f03256ad55501a151f9e5d7c6
xuetaolu <36478973+xuetaolu@users.noreply.github.com>
2023-09-05 17:29:12 +0800

    原神水底焦散逆向

---
+++ Assets/Scenes/shaderDevelop_2023_08_10/原神水底焦散/genship_water_common.hlsl
@@ -0,0 +1,30 @@
+#ifndef GENSHIP_WATER_COMMON_INCLUDED
+#define GENSHIP_WATER_COMMON_INCLUDED
+
+#include "UnityCG.cginc"
+
+struct v2f
+{
+    float4 Varying_0 : TEXCOORD0;
+    float4 Varying_1 : TEXCOORD1;
+    float4 Varying_2 : TEXCOORD2;
+    float4 Varying_3 : TEXCOORD3;
+    float4 Varying_4 : TEXCOORD4;
+    float4 vertex : SV_POSITION;
+};
+
+float4 GlslToDxClipPos(float4 clipPos) {
+    clipPos.y = -clipPos.y;
+    clipPos.z = -0.5*clipPos.z + 0.5*clipPos.w;
+    return clipPos;
+}
+
+struct appdata
+{
+    float4 vertex : POSITION;
+    float4 color : COLOR;
+    float2 uv : TEXCOORD0;
+};
+
+
+#endif
