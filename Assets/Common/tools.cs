3276cd7d6095a9b4f8c31b955e41cd0ea88536f8
xuetaolu <36478973+xuetaolu@users.noreply.github.com>
2023-09-13 17:44:04 +0800

    龟龟tools测试

---
+++ Assets/Common/tools.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class tools : MonoBehaviour
+{
+    [MenuItem("CONTEXT/TimelineClip/Open Custom Tool")]
+    private static void OpenCustomTool(MenuCommand command)
+    {
+        // TimelineClip timelineClip = (TimelineClip)command.context;
+        //
+        // if (timelineClip != null)
+        // {
+        //     // 在这里打开你的自定义工具
+        //     Debug.Log("Custom Tool Opened for TimelineClip: " + timelineClip.displayName);
+        // }
+    }
+}
