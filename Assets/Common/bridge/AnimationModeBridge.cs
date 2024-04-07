85ba02e478baf368e9d478735f82a07e6bfe3986
xuetaolu <36478973+xuetaolu@users.noreply.github.com>
2024-04-07 16:08:29 +0800

    一个一个 renderer clip

--- Assets/Common/bridge/AnimationModeBridge.cs
+++ Assets/Common/bridge/AnimationModeBridge.cs
@@ -2,7 +2,9 @@
 // @created : 2024,03,20,10:46
 // @desc:
 
+using System.Collections.Generic;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace Common.bridge
@@ -18,5 +20,7 @@ namespace Common.bridge
         {
             AnimationMode.InitializePropertyModificationForObject(target, clip);
         }
+        
+        public static List<AnimationWindow> GetAllAnimationWindows() => AnimationWindow.GetAllAnimationWindows();
     }
 }
---
+++ Assets/Scenes/shaderDevelop_2023_08_10/一个一个renderer clip/script/DummyAnimationClipSource.cs
@@ -0,0 +1,157 @@
+﻿// @author : xue
+// @created : 2024,04,07,14:19
+// @desc:
+
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+namespace Common.bridge
+{
+    public class DummyAnimationClipSource : MonoBehaviour, IAnimationClipSource, IAnimationWindowPreview
+    {
+        private static List<AnimationClip> s_EmptyList = new List<AnimationClip>();
+
+        public AnimationClip clip;
+
+        public void GetAnimationClips(List<AnimationClip> results)
+        {
+            if (clip != null)
+                results.Add(clip);
+        }
+
+        #if UNITY_EDITOR
+        [InitializeOnLoadMethod]
+        public static void InitializeOnLoad()
+        {
+            EditorApplication.update += SyncAnimationWindow;
+        }
+
+        public static void SyncAnimationWindow()
+        {
+            AnimationWindow previewingTarget = null;
+            foreach (AnimationWindow animationWindow in AnimationModeBridge.GetAllAnimationWindows())
+            {
+                if (animationWindow.previewing)
+                {
+                    previewingTarget = animationWindow;
+                    break;
+                }
+
+            }
+
+            if (previewingTarget == null)
+                return;
+            
+            foreach (AnimationWindow other in AnimationModeBridge.GetAllAnimationWindows())
+            {
+                if (other == previewingTarget)
+                    continue;
+                if (!Mathf.Approximately(other.time, previewingTarget.time))
+                {
+                    other.time = previewingTarget.time;
+                    other.Repaint();
+                }
+            }
+        }
+        #endif
+        
+        #region IAnimationWindowPreview
+
+        private List<PlayableGraph> otherPreviewGraphs = new List<PlayableGraph>();
+        
+        public void StartPreview()
+        {
+            
+        }
+
+        public void StopPreview()
+        {
+            foreach (PlayableGraph playableGraph in otherPreviewGraphs)
+            {
+                if (playableGraph.IsValid())
+                    playableGraph.Destroy();
+            }
+            otherPreviewGraphs.Clear();
+        }
+
+        public static float GetPreviewingTime()
+        {
+            foreach (AnimationWindow myAnimationWindow in AnimationModeBridge.GetAllAnimationWindows())
+            {
+                if (myAnimationWindow.previewing)
+                {
+                    return myAnimationWindow.time;
+                }
+            }
+
+            return -1;
+        }
+        
+        public void UpdatePreviewGraph(PlayableGraph graph)
+        {
+            // 其他预览 playableGraph 更新一下
+            foreach (PlayableGraph playableGraph in otherPreviewGraphs)
+            {
+                float time = GetPreviewingTime();
+                AnimationMode.SamplePlayableGraph(playableGraph, 0, time);
+            }
+        }
+
+        public Playable BuildPreviewGraph(PlayableGraph graph, Playable graphRoot)
+        {
+            // 确保清除了全部
+            StopPreview();
+
+            DummyAnimationClipSource[] others = UnityEngine.Object.FindObjectsOfType<DummyAnimationClipSource>(true);
+
+            foreach (DummyAnimationClipSource other in others)
+            {
+                if (other == this)
+                    continue;
+                PlayableGraph playableGraph = default;
+                bool success = other.OnOtherCallBuildPreviewGraph(ref playableGraph);
+                if (success)
+                    otherPreviewGraphs.Add(playableGraph);
+                    
+            }
+            
+            // 不修改当前的预览 playableGraph
+            return graphRoot;
+        }
+
+
+        public bool OnOtherCallBuildPreviewGraph(ref PlayableGraph playableGraph)
+        {
+            if (clip == null)
+                return false;
+            
+            Animator animator = GetComponent<Animator>();
+            if (animator == null)
+                return false;
+
+            Playable rootPlayable = Playable.Null;
+
+            playableGraph = PlayableGraph.Create($"PreviewGraph-{gameObject.name}");
+
+            AnimationClipPlayable clipPlayable = AnimationClipPlayable.Create(playableGraph, clip);
+            
+            AnimationLayerMixerPlayable mixerPlayable = AnimationLayerMixerPlayable.Create(playableGraph, 1);
+
+            rootPlayable = mixerPlayable;
+            
+            int num = 0;
+            mixerPlayable.ConnectInput(num++, clipPlayable, 0, 1);
+            
+            AnimationPlayableOutput output = AnimationPlayableOutput.Create(playableGraph, "ouput", animator);
+            output.SetSourcePlayable<AnimationPlayableOutput, Playable>(rootPlayable);
+            output.SetWeight<AnimationPlayableOutput>(1.0f);
+
+            return true;
+        }
+
+        #endregion
+    }
+}
